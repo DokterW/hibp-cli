ef296afefecad28492db7af4f4e5907558b72dc1
Dr. Waldijk <DokterW@users.noreply.github.com>
2018-03-01 21:21:33 +0100

    anykey

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Check if your password has been pwned.
 
-Your password will be piped through sha1sum and checked using the [range search](https://haveibeenpwned.com/API/v2#SearchingPwnedPasswordsByRange). This means your password will not be be sent to the server.
+Your password will be piped through sha1sum and checked using the [range search](https://haveibeenpwned.com/API/v2#SearchingPwnedPasswordsByRange). This means your password will not be sent to the server.
 
 ## How to install
 
@@ -15,6 +15,9 @@ Install [doghum](https://github.com/DokterW/doghum)
 * Add more features from the Have I Been Pwned [API](https://haveibeenpwned.com/API/v2).
 
 ### Changelog
+
+#### 2018-03-01
+* Fixed the TUI a bit to make it more clear and to make it ready for more features.
 
 #### 2018-02-29
 * Search your password by SHA1 range.
--- start.sh
+++ start.sh
@@ -1,31 +1,62 @@
 #!/bin/bash
-# hibp-cli v0.1
+# hibp-cli v0.2
 # Made by Dr. Waldijk
 # Have I Been Pwned CLI.
 # Read the README.md for more info, but you will find more info here below.
 # By running this script you agree to the license terms.
 # Config ----------------------------------------------------------------------------
 HIBPNAM="Have I Been Pwned (CLI)"
-HIBPVER="0.1"
+HIBPVER="0.2"
+# Install dependencies --------------------------------------------------------------
+#if [ ! -e /usr/bin/jq ]; then
+#    FNUOSD=$(cat /etc/system-release | grep -oE '^[A-Z][a-z]+\s' | sed '1s/\s//')
+#    if [ "$FNUOSD" = "Fedora" ]; then
+#        sudo dnf -y install jq
+#    else
+#        echo "You need to install jq."
+#        exit
+#    fi
+#fi
+# Functions -------------------------------------------------------------------------
+hibp_head () {
+    clear
+    echo "$HIBPNAM v$HIBPVER"
+    echo ""
+}
 # -----------------------------------------------------------------------------------
-clear
-echo "$HIBPNAM v$HIBPVER"
-echo ""
-echo "Enter password:"
-read -p "> " -s HIBPPWD
-HIBPPWD=$(echo -n "$HIBPPWD" | sha1sum | sed -r 's/([A-Za-z0-9]+).*/\1/' | tr '[:lower:]' '[:upper:]')
-HIBPPWDPRE=$(echo "$HIBPPWD" | cut -c -5)
-HIBPPWDSUF=$(echo "$HIBPPWD" | cut -c 6-)
-HIBPPWDRNG=$(curl -s "https://api.pwnedpasswords.com/range/$HIBPPWDPRE")
-HIBPPWDCHK=$(echo "$HIBPPWDRNG" | grep $HIBPPWDSUF)
-if [[ -n "$HIBPPWDCHK" ]]; then
-    HIBPPWDCHK=$(echo "$HIBPPWDCHK" | sed -r 's/.*:(.*)/\1/')
-else
-    HIBPPWDCHK="0"
-fi
-clear
-echo "$HIBPNAM v$HIBPVER"
-echo ""
-echo "Times your password has been pwned: $HIBPPWDCHK"
-read -s -n1
-clear
+while :; do
+    hibp_head
+    echo "1. Check if your password has been pwned"
+    echo "Q. Quit"
+    echo ""
+    read -p "> " -s -n1 HIBPKEY
+    case "$HIBPKEY" in
+        1)
+            hibp_head
+            echo "Enter password:"
+            read -p "> " -s HIBPPWD
+            HIBPPWD=$(echo -n "$HIBPPWD" | sha1sum | sed -r 's/([A-Za-z0-9]+).*/\1/' | tr '[:lower:]' '[:upper:]')
+            HIBPPWDPRE=$(echo "$HIBPPWD" | cut -c -5)
+            HIBPPWDSUF=$(echo "$HIBPPWD" | cut -c 6-)
+            HIBPPWDRNG=$(curl -s "https://api.pwnedpasswords.com/range/$HIBPPWDPRE")
+            HIBPPWDCHK=$(echo "$HIBPPWDRNG" | grep $HIBPPWDSUF)
+            if [[ -n "$HIBPPWDCHK" ]]; then
+                HIBPPWDCHK=$(echo "$HIBPPWDCHK" | sed -r 's/.*:(.*)/\1/')
+            else
+                HIBPPWDCHK="0"
+            fi
+            clear
+            echo "$HIBPNAM v$HIBPVER"
+            echo ""
+            echo "Times your password has been pwned: $HIBPPWDCHK"
+            echo ""
+            read -p "Press any key to continue..." -s -n1
+        ;;
+        [qQ])
+            clear
+            break
+        ;;
+        *)
+        ;;
+    esac
+done
